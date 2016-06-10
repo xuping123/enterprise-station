8d0f2bc3d0913755795ce2670627fa8d0ba13c24
xuping123 <962343525@qq.com>
2016-06-10 14:01:18 +0800

    修改数据库账号 密码

--- data/conf/db.php
+++ data/conf/db.php
@@ -7,7 +7,7 @@ return array(
     'DB_HOST' => 'localhost',
     'DB_NAME' => 'thinkcmf',
     'DB_USER' => 'root',
-    'DB_PWD' => '',
+    'DB_PWD' => '123456',
     'DB_PORT' => '3306',
     'DB_PREFIX' => 'c121mf_',
     //密钥
